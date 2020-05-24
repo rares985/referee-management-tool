393296c523238b035b6560bfe62daf1bb615521a
rares <raresmateizer@gmail.com>
2020-05-24 18:37:04 +0300

    Azure tries

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>Hello, I am OK!</p>
+    </div>
+  );
 }
 
 export default App;
