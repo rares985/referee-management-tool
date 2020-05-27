5a07b1c660076663ecc9f715422daffa39a9e825
rares985 <raresmateizer@gmail.com>
2020-05-27 17:17:10 +0300

    Connected the database, added some stubs

--- client/src/containers/LoginStub.jsx
+++ client/src/containers/LoginStub.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Button, FormGroup, FormControl, FormLabel, Card } from "react-bootstrap";
+import { Button, FormGroup, InputGroup, FormControl, FormLabel, Card } from "react-bootstrap";
 
-
+/* eslint-disable */
 const LoginStub = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isMasked, setMasked] = useState(true);
 
     function validateForm() {
         return email.length > 0 && password.length > 0;
@@ -15,13 +16,47 @@ const LoginStub = () => {
         event.preventDefault();
     }
 
+    const EyeOpenIcon = () => {
+        return (
+            <svg className="bi bi-eye-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                <path fillRule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+            </svg>
+        )
+    }
+
+    const EyeClosedIcon = () => {
+        return (
+            <svg className="bi bi-eye-slash-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z" />
+                <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829z" />
+                <path fillRule="evenodd" d="M13.646 14.354l-12-12 .708-.708 12 12-.708.708z" />
+            </svg>
+        )
+    }
+
+    const EyeIcon = (props) => {
+        if (props.isMasked)
+            return <EyeClosedIcon />
+        return <EyeOpenIcon />
+    }
+
+
+    const LockIcon = () => {
+        return (
+            <svg className="bi bi-lock-fill" width="3em" height="3em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <rect width="11" height="9" x="2.5" y="7" rx="2" />
+                <path fillRule="evenodd" d="M4.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z" />
+            </svg>
+        )
+    }
     return (
         <div className="page-container">
             <div className="login">
                 <Card>
                     <form onSubmit={handleSubmit}>
                         <div className="avatar">
-                            <i className="ion-ios-home" />
+                            <LockIcon />
                         </div>
                         <FormGroup controlId="email">
                             <FormLabel>Email</FormLabel>
@@ -32,22 +67,29 @@ const LoginStub = () => {
                                 onChange={e => setEmail(e.target.value)}
                             />
                         </FormGroup>
-                        <FormGroup controlId="password">
-                            <FormLabel>Password</FormLabel>
+                        <FormLabel>Password</FormLabel>
+                        <InputGroup className="mb-3">
                             <FormControl
-                                type="password"
+                                type={isMasked ? 'password' : 'text'}
                                 value={password}
                                 onChange={e => setPassword(e.target.value)}
                             />
-                        </FormGroup>
+                            <InputGroup.Append>
+                                <Button variant="outline-secondary"
+                                    onClick={() => setMasked(!isMasked)}>
+                                    <EyeIcon isMasked={isMasked} />
+                                </Button>
+                            </InputGroup.Append>
+                        </InputGroup>
                         <Button block disabled={!validateForm()} type="submit">
-                            Login
+                            Autentificare
                     </Button>
                     </form>
                 </Card>
             </div>
-        </div>
+        </div >
     );
 };
+/* eslint-enable */
 
 export default LoginStub;
---
+++ server/generate_db.sql
@@ -0,0 +1,80 @@
+CREATE TABLE counties (
+	id      INT         NOT NULL,
+    name    VARCHAR(20)
+	PRIMARY KEY (id)
+);
+
+INSERT INTO counties (id, name)
+VALUES 
+    (1,	'București'),
+    (2,	'Timiș'),
+    (3,	'Constanța'),
+    (4,	'Cluj'),
+    (5,	'Prahova'),
+    (6,	'Brașov'),
+    (7,	'Iași'),
+    (8,	'Ilfov'),
+    (9,	'Argeș'),
+    (10,	'Dolj'),
+    (11,	'Bihor'),
+    (12,	'Mureș'),
+    (13,	'Sibiu'),
+    (14,	'Arad'),
+    (15,	'Bacău'),
+    (16,	'Suceava'),
+    (17,	'Dâmbovița'),
+    (18,	'Galați'),
+    (19,	'Maramureș'),
+    (20,	'Alba'),
+    (21,	'Gorj'),
+    (22,	'Hunedoara'),
+    (23,	'Buzău'),
+    (24,	'Vâlcea'),
+    (25,	'Neamț'),
+    (26,	'Olt'),
+    (27,	'Satu'),
+    (28,	'Brăila'),
+    (29,	'Caraș'),
+    (30,	'Bistrița'),
+    (31,	'Harghita'),
+    (32,	'Vrancea'),
+    (33,	'Teleorman'),
+    (34,	'Botoșani'),
+    (35,	'Giurgiu'),
+    (36,	'Ialomița'),
+    (37,	'Călărași'),
+    (38,	'Vaslui'),
+    (39,	'Sălaj'),
+    (40,	'Tulcea'),
+    (41,	'Mehedinți'),
+    (42,	'Covasna');
+
+
+CREATE TABLE lots (
+	id      INT         NOT NULL,
+    name    VARCHAR(20)
+	PRIMARY KEY (id)
+);
+
+INSERT INTO lots (id, name)
+VALUES 
+    (1, 'Nedivizionar'),
+    (2, 'Divizionar A'),
+    (3, 'Divizionar B'),
+    (4, 'Divizionar C');
+
+
+CREATE TABLE categories (
+	id      INT         NOT NULL,
+    name    VARCHAR(20)
+	PRIMARY KEY (id)
+);
+
+INSERT INTO categories (id, name)
+VALUES 
+    (1, 'International'),
+    (2, 'Republican'),
+    (3, 'I'),
+    (4, 'II'),
+    (5, 'III'),
+    (6, 'IV');
