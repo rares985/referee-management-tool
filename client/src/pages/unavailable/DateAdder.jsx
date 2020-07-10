7f87cb5ce1b17bb2e6ecff98ee62256a2b06e5f6
rares985 <raresmateizer@gmail.com>
2020-07-10 15:30:46 +0300

    Fixed Dateadder alignment

--- client/src/pages/unavailable/DateAdder.jsx
+++ client/src/pages/unavailable/DateAdder.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import { KeyboardDatePicker } from '@material-ui/pickers';
 
 // eslint-disable-next-line no-unused-vars
 const useStyles = makeStyles((theme) => ({
-  root: {
+  customGrid: {
     display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'baseline',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  gridContent: {
+    marginBottom: theme.spacing(1),
+    marginTop: theme.spacing(2),
   },
 }));
 
@@ -40,46 +49,56 @@ const DateAdder = (props) => {
   };
 
   return (
-    <form onSubmit={handleAddNewDate} className={classes.root}>
-      <KeyboardDatePicker
-        margin="normal"
-        id="date-picker-dialog-start"
-        label="Data începerii"
-        format="dd/MM/yyyy"
-        value={startDate}
-        onChange={setStartDate}
-        KeyboardButtonProps={{
-          'aria-label': 'change start date',
-        }}
-      />
-      <KeyboardDatePicker
-        margin="normal"
-        id="date-picker-dialog-end"
-        label="Data încheierii"
-        format="dd/MM/yyyy"
-        value={endDate}
-        onChange={setEndDate}
-        KeyboardButtonProps={{
-          'aria-label': 'change end date',
-        }}
-      />
-      <TextField
-        autoFocus
-        variant="outlined"
-        margin="normal"
-        type="text"
-        size="small"
-        label="Motiv"
-        value={reason}
-        onChange={(e) => setReason(e.target.value)}
-        error={error}
-        helperText={error ? 'Trebuie să specificați un motiv' : ''}
-        readOnly
-      />
-
-      <Button variant="contained" color="primary" block="true" type="submit">
-        Adaugă
-      </Button>
+    <form onSubmit={handleAddNewDate}>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <KeyboardDatePicker
+            margin="normal"
+            id="date-picker-dialog-start"
+            label="Data începerii"
+            format="dd/MM/yyyy"
+            value={startDate}
+            onChange={setStartDate}
+            KeyboardButtonProps={{
+              'aria-label': 'change start date',
+            }}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <KeyboardDatePicker
+            margin="normal"
+            id="date-picker-dialog-end"
+            label="Data încheierii"
+            format="dd/MM/yyyy"
+            value={endDate}
+            onChange={setEndDate}
+            KeyboardButtonProps={{
+              'aria-label': 'change end date',
+            }}
+          />
+        </Grid>
+        <Grid item xs={3} className={classes.customGrid}>
+          <FormControl className={classes.gridContent} fullWidth error={error}>
+            <InputLabel id="demo-simple-select-outlined-label">Motiv</InputLabel>
+            <Select value={reason} onChange={(e) => setReason(e.target.value)} label="Motiv">
+              <MenuItem value="Suspendat">Suspendat</MenuItem>
+              <MenuItem value="Indisponibil">Indisponibil</MenuItem>
+            </Select>
+            {error && <FormHelperText>Trebuie să specificați un motiv</FormHelperText>}
+          </FormControl>
+        </Grid>
+        <Grid item xs={3} className={classes.customGrid}>
+          <Button
+            className={classes.gridContent}
+            variant="contained"
+            color="primary"
+            block="true"
+            type="submit"
+          >
+            Adaugă
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   );
 };
